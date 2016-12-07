d84e389a4108b00a0ce1e5a92ea74a4858a73d98
snorlaxh <cwh9702@naver.com>
2016-12-07 18:01:36 +0900

    [2016-12-07] 기본 설정 조금밖에 못함

---
+++ gulpfile.js
@@ -0,0 +1,3 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 
 let win
 
-function createWindow() { 
-    win = new BrowserWindow({width: 360, height: 600})
-    
+function createWindow() {
+    win = new BrowserWindow({ width: 360, height: 600 })
+
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
@@ -23,13 +23,13 @@ function createWindow() {
 
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin'){
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', () => {
-    if(win === null) {
+    if (win === null) {
         createWindow();
     }
 })
